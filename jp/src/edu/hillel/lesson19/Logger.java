784a1d9aa6fe9256a58ee4563f291d87addb5bf0
tvv923 <109209967+tvv923@users.noreply.github.com>
2023-10-16 08:34:56 +0300

    Delete jp/src/edu/hillel/lesson19 directory

--- jp/src/edu/hillel/lesson19/Logger.java
+++
@@ -1,6 +0,0 @@
-package edu.hillel.lesson19;
-
-public interface Logger {
-    void info(String message);
-    void debug(String message);
-}
--- jp/src/edu/hillel/lesson19/LoggingLevel.java
+++
@@ -1,5 +0,0 @@
-package edu.hillel.lesson19;
-
-public enum LoggingLevel {
-    INFO, DEBUG
-}
