cdce666f10fc148392b04c707b6ab3f8f63d19f7
tvv923 <109209967+tvv923@users.noreply.github.com>
2023-10-20 18:22:44 +0300

    Update Homework.java

--- jp/src/edu/hillel/lesson21/Homework.java
+++ jp/src/edu/hillel/lesson21/Homework.java
@@ -10,6 +10,12 @@ public class Homework {
         this.description = description;
     }
 
+    public Homework(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
     public int getId() {
         return id;
     }
