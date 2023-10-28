15817553211219ef1152b0ea40aa2906b804574a
tvv923 <109209967+tvv923@users.noreply.github.com>
2023-10-28 20:46:23 +0300

    Update Table.java

--- jp/src/edu/hillel/lesson23/Factory/Table.java
+++ jp/src/edu/hillel/lesson23/Factory/Table.java
@@ -1,6 +1,6 @@
 package edu.hillel.lesson23.Factory;
 
-class Table implements Furniture {
+public class Table implements Furniture {
     @Override
     public void assemble() {
         System.out.println("Assembling a table.");
