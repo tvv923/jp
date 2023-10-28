bf8157e696cf43a1f4086b037e8ada4e672ad75b
tvv923 <109209967+tvv923@users.noreply.github.com>
2023-10-28 20:45:47 +0300

    Update Chair.java

--- jp/src/edu/hillel/lesson23/Factory/Chair.java
+++ jp/src/edu/hillel/lesson23/Factory/Chair.java
@@ -1,6 +1,6 @@
 package edu.hillel.lesson23.Factory;
 
-class Chair implements Furniture {
+public class Chair implements Furniture {
     @Override
     public void assemble() {
         System.out.println("Assembling a chair.");
