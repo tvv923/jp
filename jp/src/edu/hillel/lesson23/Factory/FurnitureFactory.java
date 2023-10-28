30abbffbf71171e0bd6813f71552c85b1f81e960
tvv923 <109209967+tvv923@users.noreply.github.com>
2023-10-28 20:47:40 +0300

    Update FurnitureFactory.java

--- jp/src/edu/hillel/lesson23/Factory/FurnitureFactory.java
+++ jp/src/edu/hillel/lesson23/Factory/FurnitureFactory.java
@@ -1,12 +1,5 @@
 package edu.hillel.lesson23.Factory;
 
-public class FurnitureFactory {
-    public Furniture createFurniture(FurnitureType fType) {
-        return switch (fType) {
-            case CHAIR -> new Chair();
-            case TABLE -> new Table();
-            case SOFA -> new Sofa();
-            default -> throw new IllegalArgumentException("Wrong furniture type: " + fType);
-        };
-    }
+public abstract class FurnitureFactory {
+    public abstract Furniture createFurniture();
 }
